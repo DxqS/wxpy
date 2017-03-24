b2dd9e7b0906ebb2cac6e571f8b01eff0195ffed
dxq02 <353622088@qq.com>
2017-03-24 16:13:01 +0800

    product error

--- Test20170324/wxpy_test.py
+++ Test20170324/wxpy_test.py
@@ -1,7 +1,9 @@
 from wxpy import *
 
 bot = Bot()
-my_friend = bot.chats()
+my_friend = bot.friends().search('Kaiser')
+
+
 # print(my_friend)
 # my_friend.send('Hello WeChat!')
 
@@ -15,5 +17,17 @@ def print_others(msg):
 def reply_my_friend(msg):
     return 'hello,{},My Boss is busy now,please call him later.'.format(msg.sender.name)
 
+
+# 获得 Logger
+logger = get_wechat_logger(receiver=bot)
+
+# 发送警告
+logger.warning('这是一条 WARNING 等级的日志！')
+
+# 捕获可能发生的异常，并发送
+try:
+    1 / 0
+except:
+    logger.exception('又出错啦！')
 
 embed()
